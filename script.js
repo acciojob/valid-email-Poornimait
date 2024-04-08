22abd88792251fee550cd47f19fe514ae2fd14bc
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-08 18:58:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.length===0){
+		return false;
+	}
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(str);
 }
 
 // Do not change the code below.
